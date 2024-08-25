56a07ec94fda7b0f2d79312a1d8c11bdd3179f15
tan-vinh <leecoongthanhfvinh@gmail.gmail.com>
2024-08-25 21:19:30 +0900

    post bai tap

--- src/Bai1.java
+++ src/Bai1.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Bai1 {
@@ -11,7 +12,9 @@ public class Bai1 {
             int num2 = sc.nextInt();
             int thuong = num1 / num2;
             System.out.println("Thuong hai so la: " + thuong);
-        }catch (Exception e){
+        }catch (InputMismatchException ex){
+            System.out.println("hay nhap so");
+        }catch (ArithmeticException e){
             System.err.println("Khong the chia cho 0");
         }finally {
             System.out.println("Ket thuc chuong trinh");
